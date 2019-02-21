302148a4ab32e226a41533fb7040d9910358292e
unknown <ciubo_995@yahoo.com>
2019-02-21 16:42:29 +0200

    few changes on calendar and right section

--- src/app/right-character/right-character.component.ts
+++ src/app/right-character/right-character.component.ts
@@ -7,6 +7,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class rightCharacterComponent implements OnInit {
 
+  public characterSelected = 0;
+
+  public characters = [
+    {
+      name:"Natsu",
+      img:'natsu',
+      bonuses:[
+        {
+          name:"Gold",
+          bonusImg:"money",
+          actionValue:"+",
+          value:10,
+          color:"green",
+          procent:"%"
+        },
+        {
+          name:"Power",
+          bonusImg:"power",
+          actionValue:"-",
+          color:"green",
+          value:10,
+          procent:"%"
+        }
+      ]
+    },
+    {
+      name:"Lucy",
+      img:'lucy',
+      bonuses:[
+        {
+          name:"Gold",
+          bonusImg:"money",
+          actionValue:"+",
+          color:"green",
+          value:5,
+          procent:"%"
+        },
+        {
+          name:"Power",
+          bonusImg:"power",
+          actionValue:"+",
+          color:"red",
+          value:5,
+          procent:"%"
+        },
+        {
+          name:"Appeal",
+          bonusImg:"happy",
+          actionValue:"+",
+          color:"green",
+          value:40,
+          procent:"%"
+        }
+      ]
+    },
+    {
+      name:"Gray",
+      img:'gray',
+      bonuses:[
+        {
+          name:"Gold",
+          bonusImg:"money",
+          actionValue:"+",
+          color:"green",
+          value:20,
+          procent:"%"
+        },
+        {
+          name:"Power",
+          bonusImg:"power",
+          actionValue:"+",
+          color:"red",
+          value:10,
+          procent:"%"
+        },
+        {
+          name:"Appeal",
+          bonusImg:"sad",
+          actionValue:"+",
+          color:"red",
+          value:10,
+          procent:"%"
+        },
+        {
+          name:"Materials",
+          bonusImg:"materials",
+          actionValue:"+",
+          color:"green",
+          value:5,
+          procent:"%"
+        },
+      ]
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
