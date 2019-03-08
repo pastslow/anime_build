727fb0eb623801753d2b2230d4b780e8c85e93d9
unknown <ciubo_995@yahoo.com>
2019-03-08 16:18:41 +0200

    energy bar functionality

--- src/app/top-section/top-bar/top-bar.component.ts
+++ src/app/top-section/top-bar/top-bar.component.ts
@@ -8,10 +8,32 @@ import { LogicService } from 'src/app/logic.service';
 })
 export class TopBarComponent implements OnInit {
 
-  public topBarData;
-  
+  public topBarData;  
+
+  public maxEnergy = 100;
+
+  public curentEnergy = 0;
+
+  public energyConsumption = ((this.maxEnergy * this.curentEnergy) / 100);
+
+  public procentEnergy = this.energyConsumption.toString()+"%";
+
+  getConsumption(curentEnergy, totalEnergy){
+    
+    let procent = (curentEnergy / totalEnergy) * 100;
+
+    if(procent >= 100){
+      procent = 100;
+    }
+
+    return procent.toString() + "%";
+
+  }
+
   constructor(private _logicService:LogicService) { 
-    this._logicService.cast.subscribe(gameValues => this.topBarData = gameValues)
+    this._logicService.cast.subscribe(gameValues => this.topBarData = gameValues);
+    this.maxEnergy = this.topBarData.maxEnergy;
+    this.curentEnergy = this.topBarData.energy;
   }
   
   ngOnInit() {
