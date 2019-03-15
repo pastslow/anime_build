c23882e6d7e01e2f635bf80cb0fa3feda7f92973
unknown <ciubo_995@yahoo.com>
2019-03-15 14:43:39 +0200

    larger volume control

--- src/app/misc/const.ts
+++ src/app/misc/const.ts
@@ -2,77 +2,77 @@ import {Howl, Howler} from 'howler';
 
 const soundTheme = new Howl({
     src: ['assets/AUDIO/FAIRY_THEME.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const btnClick = new Howl({
     src: ['assets/AUDIO/BTN_CLICK.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const construct = new Howl({
     src: ['assets/AUDIO/CONSTRUCT.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const demolish = new Howl({
     src: ['assets/AUDIO/DEMOLISH.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const power_off = new Howl({
     src: ['assets/AUDIO/POWER_OFF.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const power_on = new Howl({
     src: ['assets/AUDIO/POWER_ON.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const ambient = new Howl({
     src: ['assets/AUDIO/AMBIENT.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const sasuke = new Howl({
     src: ['assets/AUDIO/SASUKE.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const kakashi = new Howl({
     src: ['assets/AUDIO/KAKASHI.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const itachi = new Howl({
     src: ['assets/AUDIO/ITACHI.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const kaneki = new Howl({
     src: ['assets/AUDIO/KANEKI.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const akatsuki = new Howl({
     src: ['assets/AUDIO/AKATSUKI.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const kaguya = new Howl({
     src: ['assets/AUDIO/KAGUYA.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const madara = new Howl({
     src: ['assets/AUDIO/MADARA.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 const mikasa = new Howl({
     src: ['assets/AUDIO/MIKASA.MP3'],
-    volume: 0.1
+    volume: 1
 });
 
 export{soundTheme};
